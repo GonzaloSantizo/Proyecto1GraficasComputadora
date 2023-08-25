c1003deebc620f9ce361b7e87e07964cf34139b2
GonzaloSantizo <san21504@uvg.edu.gt>
2023-08-25 17:48:03 -0600

    Final Changes

--- main.py
+++ main.py
@@ -20,16 +20,16 @@ rend.glLookAt(camPos=(0,0,0),eyePos=(0,0,-3))
 
 
 # Modelo 1
-rend.glLoadModel(filename="Squid.obj",textureName="model.bmp",translate=(2,0,-5),rotate=(0,0,0),scale=(0.25,0.25,0.25))
+rend.glLoadModel(filename="model.obj",textureName="model.bmp",translate=(2,-2,-5),rotate=(0,0,0),scale=(0.75,0.75,0.75))
 
 #Modelo 2
-rend.glLoadModel(filename="Patrick.obj",textureName="model.bmp",translate=(-2,0,-5),rotate=(0,0,0),scale=(0.5,0.5,0.5))
+rend.glLoadModel(filename="Nyanko.obj",textureName="Cat.bmp",translate=(-2,1,-5),rotate=(0,0,0),scale=(0.5,0.5,0.5))
 
 # Modelo 3
-#rend.glLoadModel(filename="Kevin.obj",textureName="model.bmp",translate=(0,0,-5),rotate=(0,0,0),scale=(1,1,1))
+rend.glLoadModel(filename="Cachorro.obj",textureName="Beagle.bmp",translate=(-1,-2,-5),rotate=(0,0,0),scale=(0.5,0.5,0.5))
 
 # Modelo 4
-rend.glLoadModel(filename="Crabs.obj",textureName="model.bmp",translate=(0,0,-5),rotate=(0,0,0),scale=(0.25,0.25,0.25))
+rend.glLoadModel(filename="Chucho.obj",textureName="Shiba.bmp",translate=(0,0,-5),rotate=(0,0,0),scale=(0.25,0.25,0.25))
 
 rend.glRender()
 rend.glFinish("output.bmp")
